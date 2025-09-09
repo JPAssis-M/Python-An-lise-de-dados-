03964b68e0c8915942101404aa317f49c6ae35b5
JPAssis-M <jpassis2106@gmail.com>
2025-09-08 22:31:58 -0300

    AULA 6 - WEBSCRAPPING
    WEBSCRAPPING

---
+++ webscrapping.py
@@ -0,0 +1,168 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import random
+import sqlite3
+import datetime
+
+# headers para simular que somos um navegador real
+#camuflagem no modo sniper| ☻
+headers = {
+    'User-Agent':'Mozill/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36(KHTML, like Gecko) Chrome/114.0 Safari/537.36'
+}
+baseURL = "https://www.adorocinema.com/filmes/melhores/"
+filmes = [] #Lista que vai armazenar os dados coletor de cada filme
+data_hoje = datetime.date.today().strftime("%d-%m-%Y")
+inicio = datetime.datetime.now()
+
+card_temp_min = 1
+card_temp_max = 3
+pag_temp_min = 2
+pag_temp_max = 4
+paginaLimite = 2 #limite de páginas
+bancoDados = "filmes.db"
+saidaCSV = f"filme_adorocinema_{data_hoje}.csv"
+
+for pagina in range(1, paginaLimite +1):
+    url = f"{baseURL}?page={pagina}"
+    print(f"Coletando dados da pagina {pagina} \n URL: {url}")
+    resposta = requests.get(url, headers=headers)
+
+    if resposta.status_code != 200:
+        print(f"Erro ao carregar a pagina {pagina}.Codigo do erro é: {resposta.status_code}")
+    soup = BeautifulSoup(resposta.text, "html.parser")
+    cards = soup.find_all("div",class_="card entity-card entity-card-list cf")
+
+for card in cards:
+        try:
+            # captura o titulo do filme e o hiperlink da pagina do filme
+            titulo_tag = card.find("a", class_="meta-title-link")
+            titulo = titulo_tag.text.strip() if titulo_tag else "N/A"
+            link = "https://www.adorocinema.com/" + titulo_tag['href'] if titulo_tag else None
+
+            #captura a nota do filme
+            nota_tag = card.find("span", class_="stareval-note")
+            nota = nota_tag.text.strip().replace(',','.') if nota_tag else "N/A"
+
+            diretor = "N/A"
+            genero_block = None
+
+            # caso exista um link, acessar a pagina individual do filme e capturar os dados.
+            if link:
+                filme_resposta = requests.get(link, headers=headers)
+                if filme_resposta.status_code == 200:
+                    filme_soup = BeautifulSoup(filme_resposta.text, "html.parser")
+
+                    #bora capturar o diretor?
+                    diretor_tag = filme_soup.find("div", class_="meta-body-item meta-body-direction meta-body-oneline")
+                    if diretor_tag:
+                        diretor = (
+                            diretor_tag.text
+                            .strip()
+                            .replace('Direção:','')
+                            .replace(',','')
+                            .replace('|','')
+                            .replace('\n',' ')
+                            .replace('\r','')
+                            .strip()
+                        )
+                    # capturar os generos
+                    genero_block = filme_soup.find('div', class_='meta-body-info')
+
+            #captura dos generos (fallback se nao acessou o link)
+            if genero_block:
+                genero_links = genero_block.find_all('a') 
+                generos = [g.text.strip() for g in genero_links]
+                categoria = ", ".join(generos[:3] if generos else "N/A")
+            else:
+                categoria = "N/A"
+            
+            # captura do ano de lançamento do filme
+            # dica: a tag span e o nome da classe é date
+            ano_tag = genero_block.find('span', class_='date') if genero_block else None
+            ano = ano_tag.text.strip() if ano_tag else "N/A"
+
+            # so adiciona o filme se todos os dados principais existirem
+            if titulo != "N/A" and link is not None and nota != "N/A":
+                filmes.append({
+                    "Titulo": titulo,
+                    "Direção": diretor,
+                    "Nota": nota,
+                    "Link": link,
+                    "Ano": ano,
+                    "Categoria": categoria
+                })
+            else:
+                print(f"Filme incompleto ou erro na coleta de dados {titulo}")
+            
+            # aguardar um tempo aleatorio para nao sobrecarregar o site
+            tempo = random.uniform(card_temp_min,card_temp_max) 
+            print(f"Tempo de Espera entre cartão: {tempo:.1f}s")
+            time.sleep(tempo)
+        except Exception as erro:
+            print(f"Erro ao processa o filme {titulo}. Erro: {erro}")
+    # antes de passar para a proxima pagina, vamos esperar um tempo
+tempo = random.uniform(pag_temp_min,pag_temp_max) 
+print(f"Tempo de Espera entre paginas: {tempo:.1f}s")
+time.sleep(tempo)
+
+# converter os dados em um dataframe dos pandas
+df = pd.DataFrame(filmes)
+print(df.head())
+
+#vamos salvar os dados em um arquivo csv
+termino = datetime.datetime.now()
+#__________________________________________
+#SQLITE: CRIAÇÃO E INSERT NO BANCO
+#__________________________________________
+with sqlite3.adapt(bancoDados) as conn: 
+    cursor = conn.cursos()
+
+    #tabela simples: link unico para evitar  a repeticao ao roda de novo
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS filmes(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    Titulo TEXT,
+                    Direcao TEXT,
+                    Nota REAL,
+                    Link TEXT UNIQUE,
+                    Ano TEXT,
+                    Categoria TEXT
+                    ''')    
+
+# INSERIR CADA FILME COLETADO
+    for filme in filmes:
+        try:
+            cursor.execute('''
+                INSERT OR IGNORE INTO filmes (Titulo, Direcao, Nota, Link, Ano, Categoria) VALUES (?,?,?,?,?,?)
+             ''',(
+                 filme['Titulo'],
+                 filme['Direcao'],
+                 float(filme[nota]) if filme [nota] !='N/A' else None,
+                 filme['Link'],
+                 filme['Ano'],
+                 filme['Categoria']
+             ))
+        except Exception as erro:
+            print (f'Erro ao inserir filme {filme[titulo]} no banco de dados. \nDetalhes: {erro}')
+    conn.commit()
+    conn.close()
+
+
+
+print("-----------------------------------------------")
+print("Dados raspados e salvos com sucesso")
+print(f"\nArquivo CSV salvo em: {saidaCSV}")
+print("\nObrigado por usar o Sistema de Bot CineBot")
+print(f"\nIniciado em: {inicio.strftime('%H:%M:%S')}")
+print(f"\nFinalizado em: {termino.strftime('%H:%M:%S')}")
+print("-----------------------------------------------")
+
+df.to_csv(saidaCSV, index=False, encoding='utf-8-sig')
+original_db = {filmes.db}
+backup_db = "filmes_backup.db"
+print(f"Backup do banco de dados criado com sucesso: {backup_db}")
+conn = sqlite3.connect("filmes.db")
+
+
